7f772e54640fb0d5663212cbc6633c337f6c9465
Rachel Lader <rachel.l.lader@gmail.com>
2015-11-06 10:21:58 -0800

    [delete] : Delete localPWD.js

--- localPWD.js
+++
@@ -1,5 +0,0 @@
-var localPWD = function() {
-  this.url = "postgres://igcprjwjtdhvfj:IRLgEsgQZw6aXwOzyXe_oomnD5@ec2-107-21-223-147.compute-1.amazonaws.com:5432/dbmg3v7j17ledn"; 
-}
-​
-module.exports = localPWD;
