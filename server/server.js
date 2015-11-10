7e431a9ba0cd2941db791b0969340ac7733a8615
Rachel Lader <RachelLader@RachelsAir.local>
2015-11-09 17:27:20 -0800

    [fix] : Adjust ng-repeat in profile.js to allow profile pictures

--- server/server.js
+++ server/server.js
@@ -531,9 +531,9 @@ app.get("/api/getFriends", function(req, res){
         }
       }).then(function(friendsArray){
         //return only the friend name
-        friendsArray = _.map(friendsArray, function(friend){
-          return friend.friend_name;
-        })
+        // friendsArray = _.map(friendsArray, function(friend){
+        //   return friend.friend_name;
+        // })
         console.log("freinds array: ", friendsArray);
         res.send(friendsArray);
       });
