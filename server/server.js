6dbb72cb8e72b3787e1b66af56fe96078d8fe127
mlu <melodylu@users.noreply.github.com>
2015-11-09 19:52:26 -0800

    AfterPres

--- server/server.js
+++ server/server.js
@@ -519,7 +519,10 @@ app.get("/api/getFriends", function(req, res){
           user_id: user.id
         }
       }).then(function(friendsArray){
-
+        //return only the friend name
+        friendsArray = _.map(friendsArray, function(friend){
+          return friend.friend_name;
+        })
         console.log("freinds array: ", friendsArray);
         res.send(friendsArray);
       });
